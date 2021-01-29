d398902e0cbecf280cf52bcc26f9d61f5e01d549
vcjatin <jatin.raghav@mail.vinove.com>
2021-01-29 10:37:57 +0530

    create node server

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+
+
+// SERVER 🙈
+const server = http.createServer((req, res) => {
+     const path = req.url;
+    if (path === '/' || path === '/overview') {
+        res.end("this is overview!")
+    } else {
+        res.writeHead(404, {
+            'Content-type': "text/html",
+            'my-own-header': 'hello world'
+        });
+
+        res.end('<h1> Page not Found! </h1>');
+
+
+    }
+    console.log(path);
+})
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listensing from server! 👂')
+
+})
