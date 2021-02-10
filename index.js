55a928802896949bfbf25abea91f4a779a601fdc
vcjatin <jatin.raghav@mail.vinove.com>
2021-02-10 12:00:38 +0530

    changes for test

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-const http = require('http');
-const url = require('url');
+const { fstat } = require("fs");
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
 
-
+// access file data
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 
 // SERVER 🙈
 const server = http.createServer((req, res) => {
-     const path = req.url;
-    if (path === '/' || path === '/overview') {
-        res.end("this is overview!")
-    } else {
-        res.writeHead(404, {
-            'Content-type': "text/html",
-            'my-own-header': 'hello world'
-        });
+  const path = req.url;
+  if (path === "/" || path === "/overview") {
+    res.end("this is overview!");
+  } else if (path === "/api") {
+    // const folderPath = './dev-data/data.json'
+    // const productData = fs.readdirSync(folderPath)
+    //   console.log(productData);
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello world",
+    });
 
-        res.end('<h1> Page not Found! </h1>');
+    res.end("<h1> Page not Found! </h1>");
+  }
+  console.log(path);
+});
 
-
-    }
-    console.log(path);
-})
-
-server.listen(8000, '127.0.0.1', () => {
-    console.log('Listensing from server! 👂')
-
-})
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listensing from server! 👂");
+});
