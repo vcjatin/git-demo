a8985d5616593a6a1f91153d63aff82f5752b3d4
vcjatin <jatin.raghav@mail.vinove.com>
2021-02-10 14:16:40 +0530

    changes for build test success

--- index.js
+++ index.js
@@ -25,12 +25,10 @@ const server = http.createServer((req, res) => {
       "Content-type": "text/html",
       "my-own-header": "hello world",
     });
-  }
 
     res.end("<h1> Page not Found! </h1>");
   }
   console.log(path);
-  
 });
 
 server.listen(8000, "127.0.0.1", () => {
